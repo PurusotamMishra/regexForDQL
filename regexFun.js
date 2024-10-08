27d91a422ab129dac3c4074d9b53c1ee00594b2f
Purusotam Mishra <purusotam.mishra@net-mon.net>
2024-10-08 16:00:46 +0530

    Added list of functions in consts.js and select, having, groupby and where clause validation

--- regexFun.js
+++ regexFun.js
@@ -9,3 +9,83 @@ regexFun = "hello"
 
 
 // all = `(\s+\|\s+\b[dD][uU][rR][aA][tT][iI][oO][nN]\b\s+\d{1,}[mhdwM]|\s+\|\s+\b[dD][uU][rR][aA][tT][iI][oO][nN]\b\s+[fF][rR][oO][mM]\s+\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}\s+\b[tT][oO]\b\s+\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}\b)|(\b\s+\|\s+\b[gG][rR][oO][uU][pP][bB][yY]\s+[\w+\,]+)|(\b\s+\|\s+\b[tT][iI][mM][eE][sS][lL][iI][cC][eE]\s+(1m|1h))|(\bstream\b[\=]([\w]+|[*]))`
+
+
+// stream=firewall | select srcip ,  dstip as  col1  ,  length(sourcename) , length( user ) as col2
+// stream=firewall | select count( evtlen )
+
+
+
+    // // if groupby dne in the dql query
+    // if (gbyQuery === -1) {
+    //     // case I: does not contains functions that require groupby -- aggregate function
+    //     // case II: functions' syntax that does not require groupby
+    //     // case III: select contains single or multi fields 
+
+    //     if (/distinct_count\s*\(|count\s*\(|count_if\s*\(|distinct\s*\(|length\s*\(|avg\s*\(/i.test(query)) {
+    //         // console.log('Functions should not exists without groupby')
+    //         return { isValid: false, message: "Functions doesn't exists without groupby clause" }
+    //     }
+    //     else {
+    //         if (!/^\b[sS][eE][lL][eE][cC][tT]\b\s+(((\w+\s+as\s+\w+)|(\w+))(?:\s*,\s*(\w+(?:\s+as\s+\w+)?))*)$/.test(query.trim())) {
+    //             // check for columns syntax only: single and multiple fields with "as col_name"
+    //             let match = /\b[sS][eE][lL][eE][cC][tT]\b\s+(((\w+\s+as\s+\w+)|(\w+))(?:\s*,\s*(\w+(?:\s+as\s+\w+)?))*)/.exec(query.trim())
+    //             if (!match) {
+    //                 return { isValid: false, message: 'Error near select clause' }
+    //             }
+    //             const matchedPart = match[0]
+    //             const mismatchStart = matchedPart.length
+    //             return { isValid: false, message: `Error near 'select' clause at: ${query.trim().slice(mismatchStart)}` }
+    //         }
+    //     }
+    // }
+    // // if groupby exists in dql query
+    // else {
+    //     // case I: checks for functions that require groupby 
+    //     // case II: functions' syntax that does not require groupby
+    //     // case III: matches all the fields with grpby fields
+    //     if (/distinct_count\s*\(|count\s*\(|count_if\s*\(|distinct\s*\(|length\s*\(|avg\s*\(/i.test(query)) {
+    //         for (let i = 0; i < subFields.length; i++) {
+    //             if (/distinct_count\s*\(|count\s*\(|count_if\s*\(|distinct\s*\(|length\s*\(|avg\s*\(/i.test(subFields[i].trim().split('as')[0])) {
+    //                 validity = selectSubDirectiveCheck(subFields[i], i)
+    //                 if (validity.isValid) {
+    //                     if (!gbyFields.includes(validity.message.trim().split('as')[0])) {
+    //                         validity = { isValid: false, message: `Error at 'select' clause: ${validity.message.trim().split('as')[0]} is neither an aggregate function nor present in groupby clause!` }
+    //                     }
+    //                     if (!validity.isValid) break
+    //                 }
+    //                 else break
+    //             }
+    //             else {
+    //                 field = subFields[i]
+    //                 if (!gbyFields.includes(field.trim().split('as')[0])) {
+    //                     validity = { isValid: false, message: `Error at 'select' clause: ${field.trim().split('as')[0]} is neither an aggregate function nor present in groupby clause!` }
+    //                 }
+    //                 if (!validity.isValid) break
+    //             }
+    //         }
+    //         return validity
+    //         // return { isValid: false, message: "Functions doesn't exists without groupby clause" }
+    //     }
+    //     else {
+    //         if (!/^\b[sS][eE][lL][eE][cC][tT]\b\s+(((\w+\s+as\s+\w+)|(\w+))(?:\s*,\s*(\w+(?:\s+as\s+\w+)?))*)$/.test(query.trim())) {
+    //             // check for columns syntax only: single and multiple fields with "as col_name"
+    //             let match = /\b[sS][eE][lL][eE][cC][tT]\b\s+(((\w+\s+as\s+\w+)|(\w+))(?:\s*,\s*(\w+(?:\s+as\s+\w+)?))*)/.exec(query.trim())
+    //             if (!match) {
+    //                 return { isValid: false, message: 'Error near select clause' }
+    //             }
+    //             const matchedPart = match[0]
+    //             const mismatchStart = matchedPart.length
+    //             return { isValid: false, message: `Error near 'select' clause at: ${query.trim().slice(mismatchStart)}` }
+    //         }
+    //         else {
+    //             // syntax is correct, now check for column names matching with groupby clause
+    //             subFields.forEach(field => {
+    //                 if (!gbyFields.includes(field.trim().split('as')[0])) {
+    //                     validity = { isValid: false, message: `Error at 'select' clause: ${field.trim().split('as')[0]} is neither an aggregate function nor present in groupby clause!` }
+    //                 }
+    //             });
+    //             return validity
+    //         }
+    //     }
+    // }
--- validateQuery.js
+++ validateQuery.js
@@ -1,4 +1,6 @@
 
+const { pipes, aggregateFunctions, allFunctions, nonAggregateFunctions } = require('./consts')
+
 const queryPattern = {
     stream: /\b[sS][tT][rR][eE][aA][mM]\b\s*=\s*\w+(?:,\s*\w+)*/,
     timeslice: /\|\s*\btimeslice\b/ig,
@@ -9,9 +11,6 @@ const queryPattern = {
     having: /\|\s*\bhaving\b/ig,
     all: /\btimeslice\b|\bduration\b|\blimit\b|\bfirst\b|\blast\b|\bgroupby\b|\bselect\b|\bhaving\b/i,
 }
-
-const pipes = ['stream', 'timeslice', 'duration', 'limit', 'first', 'last', 'groupby', 'having', 'select']
-const selectSubDirectives = ['distinct_count', 'count', 'count_if', 'distinct', 'length', 'avg']
 
 const checkLimitFirstLast = (query) => {
     let validity = {
@@ -86,79 +85,192 @@ const checkDuration = (query) => {
     return validity
 }
 
-const checkGroupby = (query) => {
+const checkForStreamsAndWhere = (query) => {
+    let queryArr = query.split(/(where)/i)
+
+    // streams' name validation (multi streams)
+    let streams = queryArr[0].trim().split(/stream\s*=/i).filter(str => str !== '' && str !== null && str !== undefined)
+    streams = streams[0].trim().split(',')
+
+    for (let i = 0; i < streams.length; i++) {
+        if (!/^\w+$/m.test(streams[i].trim())) {
+            return { isValid: false, message: `Error at 'streams': Invalid stream name, found ${streams[i]}` }
+        }
+    }
+
+    //where clause validation
+    const operators = ['<=', '>=', '=', '!=', '<>', '<', '>']
+    let whereQuery = queryArr[2].trim().split(/\sand\s|\sor\s/i)
+    for (let i = 0; i < whereQuery.length; i++) {
+        let opers = whereQuery[i].trim().split(/(<=|>=|=|!=|<>|<|>)/).filter((op) => (op !== undefined && op !== null && !/^\s*$/m.test(op)))
+        if (opers.length === 0 || opers[0] === '') {
+            return { isValid: false, message: `Error at 'where': Missing operand` }
+        } else if (!/^\w+$/m.test(opers[0].trim())) {
+            return { isValid: false, message: `Error at 'where': Invalid operand, found "${opers[0]}"` }
+        } else if (opers[1] === undefined) {
+            return { isValid: false, message: `Error at 'where': Missing 'Comparison Operators(= < > >= <= != <>)'` }
+        } else if (!operators.includes(opers[1].trim())) {
+            return { isValid: false, message: `Error at 'where': Missing 'Comparison Operators(= < > >= <= != <>)', found "${opers[1]}"` }
+        } else if (opers[2] === undefined) {
+            return { isValid: false, message: `Error at 'where': Missing operand` }
+        } else if (!/^(?:['"]|)\s*\w+\s*(?:['"]|)$/m.test(opers[2].trim())) {
+            return { isValid: false, message: `Error at 'where': Invalid operand, found "${opers[2]}"` }
+        }
+
+    }
+    return { isValid: true, message: `Valid Query!` }
+}
+
+const checkGroupby = (query, gbyCol) => {
     let validity = {
         isValid: false,
         message: "Invalid Query",
     }
-    if (!/\b[gG][rR][oO][uU][pP][bB][yY]\b\s*\w+(?:,\s*\w+)*./.test(query)) {
-        return { isValid: false, message: 'Error at "groupby"!' }
+
+    let gbyFields = query.trim().split(/groupby/i).join("").split(',')
+
+    for (let i = 0; i < gbyFields.length; i++) {
+        if (/^([\w|~!=%&*+-\/<>^]+)\s*\(\s*.+\s*\)$/.test(gbyFields[i].trim())) {
+            //check if the function is not a aggregate function but is present in all function
+            let fun = /^([\w|~!=%&*+-\/<>^]+)\s*\(\s*.+\s*\)$/.exec(gbyFields[i].trim())[1]
+            if (aggregateFunctions.includes(fun)) {
+                return { isValid: false, message: `Error at 'groupby: "${fun}" is an aggregate function and cannot be with groupby clause!` }
+            } else if (allFunctions.includes(fun)) {
+                gbyCol.push(gbyFields[i].split(" ").join(""))
+            } else {
+                return { isValid: false, message: `Error at 'groupby': "${fun}" is not a valid function!` }
+            }
+        }
+        else if (/^[a-zA-Z]+$/g.test(gbyFields[i].trim())) {
+            gbyCol.push(gbyFields[i].trim())
+        }
+        else {
+            return { isValid: false, message: `Error at 'groupby': column name "${gbyFields[i].trim()}" is INVALID!` }
+        }
     }
+
     validity = { isValid: true, message: "Valid Query" }
     return validity
 }
 
-const checkHaving = (query) => {
+const checkHaving = (query, gbyCol, selCol) => {
     let validity = {
         isValid: false,
         message: "Invalid Query",
     }
+    const operators = ['<=', '>=', '==', '!=', '<', '>']
+
+    let havCond = query.trim().split(/having/i).join("").split(/\sand\s|\sor\s/i)
+
+    for (let i = 0; i < havCond.length; i++) {
+        let opers = havCond[i].trim().split(/(<=|>=|==|!=|<|>)/).filter((op) => (op !== undefined && op !== null && !/^\s*$/m.test(op)))
+        if (opers.length === 0 || opers[0] === '') {
+            return { isValid: false, message: `Error at 'having': Missing operand` }
+        } else if (!/^\w+$/m.test(opers[0].trim())) {
+            return { isValid: false, message: `Error at 'having': Invalid operand, found "${opers[0]}"` }
+        } else if (opers[1] === undefined) {
+            return { isValid: false, message: `Error at 'having': Missing 'Comparison Operators(== < > >= <= !=)'` }
+        } else if (!operators.includes(opers[1].trim())) {
+            return { isValid: false, message: `Error at 'having': Missing 'Comparison Operators(== < > >= <= !=)', found "${opers[1]}"` }
+        } else if (opers[2] === undefined) {
+            return { isValid: false, message: `Error at 'having': Missing operand` }
+        } else if (!/^\w+$/m.test(opers[2].trim())) {
+            return { isValid: false, message: `Error at 'having': Invalid operand, found "${opers[2]}"` }
+        } else if (opers[3] && !operators.includes(opers[3].trim())) {
+            return { isValid: false, message: `Error at 'having': Missing operand, found "${opers[3]}"` }
+        } else if (opers[3] !== undefined && opers[4] === undefined) {
+            return { isValid: false, message: `Error at 'having': Missing operand` }
+        } else if (opers[3] !== undefined && !/^\w+$/m.test(opers[4].trim())) {
+            return { isValid: false, message: `Error at 'having': Invalid operand, found ${opers[4]}` }
+        } else if (opers.length > 5) {
+            return { isValid: false, message: `Error at 'having': Unknown found "${opers[5]}"` }
+        }
+    }
+
     if (!/\b[hH][aA][vV][iI][nN][gG]\b\s*\w+(?:,\s*\w+)*./.test(query)) {
         return { isValid: false, message: 'Error at "having"!' }
     }
     validity = { isValid: true, message: "Valid Query" }
     return validity
 }
-/*
-count()
-count_if()
-distinct()
-length()
-avg()
-*/
-const selectSubDirectiveCheck = (query, fun) => {
-    if (fun === 'count') {
-        // /\b[sS][eE][lL][eE][cC][tT]\b\s*((((\w+\s*,\s*\bcount\s*[\(]\s*\w+\s*[\)])|((\bcount\s*[\(]\s*\w+\s*[\)])))((\s+[aA][sS]\s+\w+)|(\s*)))|((\w+\s*)(?:,\s*\w+))|(\w+\s*))/
-        if (query.includes('count') && !/\b[sS][eE][lL][eE][cC][tT]\b\s*((\w+\s*(?:,\s*\w+))|(\bcount[\(]\s*\w+\s*[\)])((\s+[aA][sS]\s+\w+)|(\s*)))/.test(query)) {
-            return { isValid: false, message: 'Error at "count" function in "select"!' }
-        }
-    }
-}
 
-const checkSelect = (query) => {
+const checkSelect = (query, gbyCol, selCol) => {
     let validity = {
         isValid: false,
         message: "Invalid Query",
     }
 
-    // if groupby, either should matches with the ones selected in groupby or should be one of the functions 
-    if (!/\b[sS][eE][lL][eE][cC][tT]\b\s*\w+(?:,\s*\w+)/.test(query)) {
-        return { isValid: false, message: 'Error at "select"!' }
+    let selFields = query.trim().split(/select/i).join(" ").trim().split(',')
+    if (gbyCol.length === 0) { //groupby clause is not present in the queryString
+        let agg = []
+        for (let i = 0; i < selFields.length; i++) {
+            let tempArr = selFields[i].trim().split(/\sas\s/i)
+            selCol.push({ 'col': tempArr[0], 'alias': tempArr[1] ? tempArr[1] : '', fun: '', agg: false })
+            if (/^([\w|~!=%&*+-\/<>^]+)\s*\(\s*.+\s*\)$/.test(tempArr[0].trim())) {
+                let fun = /^([\w|~!=%&*+-\/<>^]+)\s*\(\s*(.+)\s*\)$/.exec(tempArr[0].trim())
+                if (allFunctions.includes(fun[1])) {
+                    if (aggregateFunctions.includes(fun[1])) {
+                        agg.push(tempArr[0])
+                        selCol[i].agg = true
+                    }
+                    selCol[i].fun = fun[1]
+                    selCol[i].col = fun[2]
+                }
+                else {
+                    return { isValid: false, message: `Error at 'select': "${fun}" is not a valid function!` }
+                }
+            } else if (/^[a-zA-Z]+$/.test(tempArr[0].trim())) {
+                continue
+            }
+            else {
+                return { isValid: false, message: `Error at 'select': column name "${tempArr[0].trim()}" is INVALID!` }
+            }
+        }
+        if (agg.length !== 0 && agg.length !== selCol.length) {
+            for (let j = 0; j < selCol.length; j++) {
+                if (!agg.includes(selCol[j]['col'])) {
+                    return { isValid: false, message: `Error at 'select': "${selCol[j]['col']}" is not an aggregate function` }
+                }
+            }
+        }
     }
-    selectSubDirectives.forEach((fun) => (
-        query.includes(fun) ? selectSubDirectiveCheck(query, fun) : null
-    ))
-    // else if (query.includes('distinct_count') && !/\b[sS][eE][lL][eE][cC][tT]\b\s*((\w+(?:,\s*\w+))|(distinct_count\(\w+\)\s*as\s*\w+))./.test(query)) {
-    //     return { isValid: false, message: 'Error at "distinct_count" function in "select"!' }
-    // }
-    // else if (query.includes('count') && !/\b[sS][eE][lL][eE][cC][tT]\b\s*((\w+\s*(?:,\s*\w+))|(\bcount[\(]\s*\w+\s*[\)])((\s+[aA][sS]\s+\w+)|(\s*)))/.test(query)) {
-    //     return { isValid: false, message: 'Error at "count" function in "select"!' }
-    // }
-    // else if (query.includes('count_if') && !/\b[sS][eE][lL][eE][cC][tT]\b\s*((\w+(?:,\s*\w+))|(count_if\(\w+\)\s*as\s*\w+))./.test(query)) {
-    //     return { isValid: false, message: 'Error at "count_if" function in "select"!' }
-    // }
-    // else if (query.includes('distinct') && !/\b[sS][eE][lL][eE][cC][tT]\b\s*((\w+(?:,\s*\w+))|(distinct\(\w+\)\s*as\s*\w+))./.test(query)) {
-    //     return { isValid: false, message: 'Error at "distinct" function in "select"!' }
-    // }
-    // else if (query.includes('length') && !/\b[sS][eE][lL][eE][cC][tT]\b\s*((\w+(?:,\s*\w+))|(length\(\w+\)\s*as\s*\w+))./.test(query)) {
-    //     return { isValid: false, message: 'Error at "length" function in "select"!' }
-    // }
-    // else if (query.includes('avg') && !/\b[sS][eE][lL][eE][cC][tT]\b\s*((\w+(?:,\s*\w+))|(avg\(\w+\)\s*as\s*\w+))./.test(query)) {
-    //     return { isValid: false, message: 'Error at "avg" function in "select"!' }
-    // }
-    // else {
-    //     return { isValid: false, message: 'Error: the function is not defined in "select"!' }
-    // }
+    else { // groupby clause is present in the queryString
+        let nonAgg = []
+        for (let i = 0; i < selFields.length; i++) {
+            let tempArr = selFields[i].trim().split(/\sas\s/i)
+            selCol.push({ 'col': tempArr[0], 'alias': tempArr[1] ? tempArr[1] : '', fun: '', agg: false })
+            if (/^([\w|~!=%&*+-\/<>^]+)\s*\(\s*.+\s*\)$/.test(tempArr[0].trim())) {
+                let fun = /^([\w|~!=%&*+-\/<>^]+)\s*\(\s*(.+)\s*\)$/.exec(tempArr[0].trim())
+                if (allFunctions.includes(fun[1])) {
+                    if (aggregateFunctions.includes(fun[1])) {
+                        selCol[i].agg = true
+                    } else {
+                        nonAgg.push(fun[1])
+                    }
+                    selCol[i].fun = fun[1]
+                    selCol[i].col = fun[2]
+                }
+                else {
+                    return { isValid: false, message: `Error at 'select': "${fun}" is not a valid function!` }
+                }
+            } else if (/^[a-zA-Z]+$/.test(tempArr[0].trim())) {
+                continue
+            }
+            else {
+                return { isValid: false, message: `Error at 'select': column name "${tempArr[0].trim()}" is INVALID!` }
+            }
+        }
+
+        if (nonAgg.length !== 0) {
+            for (let j = 0; j < selFields.length; j++) {
+                if (nonAgg.includes(selCol[j]['fun'])) {
+                    if (!gbyCol.includes(selCol[j]['col']) && !aggregateFunctions.includes(selCol[j]['col'])) {
+                        return { isValid: false, message: `Error at 'select': "${selCol[j]['col']}" is neither present in 'groupby' clause nor is an aggregate functions!` }
+                    }
+                }
+            }
+        }
+    }
 
     validity = { isValid: true, message: "Valid Query" }
     return validity
@@ -186,18 +298,25 @@ const checkForNonRepeatability = (queryString) => {
     else return { isValid: true, message: 'No repeatance' }
 }
 
-const checkForIncorrectPipes = (queryArray) => {
+const checkForIncorrectPipes = (queryArray, pipesIndex) => {
     for (const i in queryArray) {
         if (queryPattern.stream.test(queryArray[i]))
             continue
         let query = queryArray[i].trim().split(' ')
         if (!queryPattern.all.test(query[0]))
             return { isValid: false, message: `The pipe ${query[0]} is not a valid in DQL!` }
+        else if (queryArray[i].toLowerCase().includes('groupby')) {
+            pipesIndex['groupby'] = i
+        }
+        else if (queryArray[i].toLowerCase().includes('having')) {
+            pipesIndex['having'] = i
+        }
+        else if (queryArray[i].toLowerCase().includes('select')) {
+            pipesIndex['select'] = i
+        }
     }
     return { isValid: true, message: 'All pipe functions are' }
 }
-
-
 
 function validateQuery(queryString) {
 
@@ -206,80 +325,103 @@ function validateQuery(queryString) {
         message: "Invalid Query",
     }
 
+    var pipesIndex = {
+        'groupby': -1,
+        'select': -1,
+        'having': -1,
+    }
+
 
     if (queryPattern.stream.test(queryString)) {
         const queryArray = queryString.split("|")
-        // console.log(queryArray)
 
-        validQ = checkForIncorrectPipes(queryArray)
+        validQ = checkForIncorrectPipes(queryArray, pipesIndex)
         if (!validQ.isValid) {
             return validQ
         }
+
         //check for non-repeatability of pipes
         validQ = checkForNonRepeatability(queryString)
         if (!validQ.isValid) {
             return validQ
         }
 
+        validQ = checkForStreamsAndWhere(queryArray[0])
+        if (!validQ.isValid)
+            return validQ
 
+        var gbyCol = []
+        var selCol = []
+        if (pipesIndex['groupby'] !== -1) {
+            validQ = checkGroupby(queryArray[pipesIndex['groupby']], gbyCol)
+            if (!validQ.isValid) {
+                return validQ
+            }
+        }
+        if (pipesIndex['select'] !== -1) {
+            validQ = checkSelect(queryArray[pipesIndex['select']], gbyCol, selCol)
+            if (!validQ.isValid) {
+                return validQ
+            }
+        }
+        if (pipesIndex['having'] !== -1) {
+            validQ = checkHaving(queryArray[pipesIndex['having']], gbyCol, selCol)
+            if (!validQ.isValid) {
+                return validQ
+            }
+        }
+
+        if (!validQ.isValid)
+            return validQ
         for (const ind in queryArray) {
             if (queryPattern.stream.test(queryArray[ind]))
                 continue
             if (queryArray[ind].toLowerCase().includes('limit') || queryArray[ind].includes('first') || queryArray[ind].includes('last')) {
                 validQ = checkLimitFirstLast(queryArray[ind])
-                continue
+                if (validQ.isValid) continue
+                else break
             }
             if (queryArray[ind].toLowerCase().includes('timeslice')) {
                 validQ = checkTimeslice(queryArray[ind])
-                continue
+                if (validQ.isValid) continue
+                else break
             }
             if (queryArray[ind].toLowerCase().includes('duration')) {
                 validQ = checkDuration(queryArray[ind])
+                if (validQ.isValid) continue
+                else break
+            }
+            if (queryArray[ind].toLowerCase().includes('having')) {
+                // validQ = checkHaving(queryArray[ind])
+                // if (validQ.isValid) continue
+                // else break
                 continue
             }
             if (queryArray[ind].toLowerCase().includes('groupby')) {
-                validQ = checkGroupby(queryArray[ind])
                 continue
-            }
-            if (queryArray[ind].toLowerCase().includes('having')) {
-                validQ = checkHaving(queryArray[ind])
-                continue
+                // validQ = checkGroupby(queryArray[ind])
+                // if (validQ.isValid) continue
+                // else break
             }
             if (queryArray[ind].toLowerCase().includes('select')) {
-                validQ = checkSelect(queryArray[ind])
+                // if (pipesIndex['groupby'] !== -1)
+                //     validQ = checkSelect(queryArray[ind], queryArray[pipesIndex['groupby']])
+                // else
+                //     validQ = checkSelect(queryArray[ind], -1)
+                // if (validQ.isValid) continue
+                // else break
                 continue
             }
-            console.log(validQ)
         }
-        //  all pipe functions should only come once
-        // if (queryPattern.duration.test(query)) {
-
-        //     let syntax2 = /\b[dD][uU][rR][aA][tT][iI][oO][nN]\b\s+\d{1,}[mhdwM]/
-        //     let syntax1 = /\b[dD][uU][rR][aA][tT][iI][oO][nN]\b\s+[fF][rR][oO][mM]\s+\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}\s+\b[tT][oO]\b\s+\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}/
-        //     // if (!syntax1.test(query) || !syntax2.test(query)) {
-        //     if (!/\b[dD][uU][rR][aA][tT][iI][oO][nN]\b\s+\d{1,}[mhdwM]|\b[dD][uU][rR][aA][tT][iI][oO][nN]\b\s+[fF][rR][oO][mM]\s+\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}\s+\b[tT][oO]\b\s+\d{4}-\d{2}-\d{2}T\d{1,2}:\d{2}:\d{2}/g.test(query)) {
-        //         return { isValid: false, message: 'For duration pipe, please write "1m" or "from date&time to date&time' }
-        //     }
-        //     else validQ = { isValid: true, message: "Valid Query" }
-        // }
-        // if (queryPattern.groupby.test(query)) {
-        //     // if(/groupby(?:\s+\w+)(?:,\s*\w+)*/)
-        //     // validQ =  { isValid: true, message: 'Query contains groupby' }
-        //     // return {isValid: true, message: "Valid Query"}
-        //     console.log("Groupby is there")
-
-        // }
-        return validQ
+        if (!validQ.isValid) {
+            return validQ
+        }
+        else return { isValid: true, message: `Valid Query` }
     }
     else {
         return { isValid: false, message: 'Invalid Query: Stream Missing' }
     }
 }
-
-
-
-//  groupby more discretely
-//  duration more cases
 
 
 module.exports = validateQuery;
